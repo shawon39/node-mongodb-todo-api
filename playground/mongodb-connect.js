331e00a8f1789822e75e7018eaec55e1e9ff7fcc
shawon <shshawon39@gmail.com>
2018-04-28 23:49:17 +0600

    Init Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,51 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user = {name: 'shawon', age: 24 };
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB Server!');
+    }
+    console.log('Connected to MongoDB Server!');
+
+    var db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //
+    //     text: 'Something to do',
+    //     completed: false
+    //
+    // }, (err, result) => {
+    //
+    //     if(err) {
+    //         return console.log('Unable to Insert Todo!', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //
+    // });
+
+    db.collection('Users').insertOne({
+
+        name: 'Shawon',
+        age: 23,
+        location: 'Dhaka'
+
+    }, (err, result) => {
+
+        if(err) {
+            return console.log('Unable to insert Tod', err);
+        }
+        console.log(result.ops[0]._id);
+        //console.log(result.ops[0]._id.getTimestamp());
+
+    });
+
+    client.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,48 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB Server!');
+    }
+    console.log('Connected to MongoDB Server!');
+
+    var db = client.db('TodoApp');
+
+    // db.collection('Todos').find({_id: new ObjectID('5ae367fda48c0d26480bc08c')}).toArray().then((docs) => {
+    //     console.log('Todos....');
+    //     console.log(JSON.stringify(docs, undefined, 4));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos');
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos');
+    // });
+
+    // Challange
+
+    db.collection('Users').find({name: 'Shawon'}).count().then((count) => {
+        console.log(`Users: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch todos');
+    });
+
+    //client.close();
+});
+
+
+
+
+
+
+
+
+
+
+
+//
