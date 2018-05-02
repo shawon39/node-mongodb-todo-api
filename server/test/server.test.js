a3668eabf7e947a8b4110392d93f4f57243896c2
shawon <shshawon39@gmail.com>
2018-05-02 15:02:23 +0600

    working with testing delete data

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -102,11 +102,50 @@ describe('GET /todos/:id', () => {
         request(app)
           .get('/todos/123abc')
           .expect(404)
-          .end(done)
+          .end(done);
     });
 });
 
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
 
+        request(app)
+          .delete(`/todos/${hexId}`)
+          .expect(200)
+          .expect((res) => {
+             expect(res.body.todo._id).toBe(hexId);
+          })
+          .end((err, res) => {
+             if(err) {
+                 return done(err);
+             }
+
+             Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist();
+                done();
+            }).catch((err) => done(err));
+          });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+        //var hexId = todos[1]._id.toHexString();
+
+        request(app)
+          .get(`/todos/${hexId}`)
+          .expect(404)
+          .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+          .get('/todos/123abc')
+          .expect(404)
+          .end(done);
+    });
+
+});
 
 
 
