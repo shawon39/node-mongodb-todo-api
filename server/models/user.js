5cff8a519a24f168d9abf2257e4481efaa3a5d02
shawon <shshawon39@gmail.com>
2018-05-24 01:36:49 +0600

    hashing password

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 // *** Security & Authentication
 
@@ -37,7 +38,6 @@ var UserSchema = new mongoose.Schema({
 UserSchema.methods.toJSON  = function () {
     var user = this;
     var userObject = user.toObject();
-    console.log(userObject);
     return _.pick(userObject, ['_id', 'email',]);
 }
 
@@ -69,6 +69,21 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if( user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 var User = mongoose.model('User', UserSchema);
 
